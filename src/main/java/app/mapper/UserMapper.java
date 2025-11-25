f8ac1443c7cfaca8870d6197da7415376e0cb589
Raichl <andfreyka.020299@gmail.com>
2025-11-25 09:26:56 +0300

    review fix

--- src/main/java/app/mapper/UserMapper.java
+++ src/main/java/app/mapper/UserMapper.java
@@ -14,6 +14,7 @@ public class UserMapper {
                 entity.getAge()
         );
     }
+
     public static User toEntity(UserDto userDto){
         return new User(userDto.getName(), userDto.getEmail(), userDto.getAge());
     }
